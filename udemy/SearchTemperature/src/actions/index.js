78df789df58dc5184d60e3ca30c15afb772e5961
YoonwooLee <lyw31136@tmon.co.kr>
2018-11-05 18:13:22 +0900

    [Udemy] apply axios(ajax)

--- udemy/SearchTemperature/src/actions/index.js
+++ udemy/SearchTemperature/src/actions/index.js
@@ -1,1 +1,16 @@
+import axios from 'axios';
+
 const API_KEY = 'a95045330d2c32eabd568f273709ee39';
+const API_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WHETHER = 'FETCH_WHETHER';
+
+export function fetchWhether(city) {
+  const url = `${API_URL}&q=${city},us`;
+  const request = axios.get(url);
+
+  return {
+    type: FETCH_WHETHER,
+    payload: request
+  };
+}
